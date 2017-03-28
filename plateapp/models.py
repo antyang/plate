f5591d142ee0b07e3a6e601e890b13ce0378303c
antyang <mrant0ny@gmail.com>
2017-03-28 14:18:53 -0700

    Add registration form

--- plateapp/models.py
+++ plateapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# OneToOne field:
+  # restaurant belong to 1 owner, 1 owner only have 1 restaurant
+# CASCADE:
+  # once you delete user, you delete the restaurant as well
+class Restaurant(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='restaurant')
+  name = models.CharField(max_length=500)
+  phone = models.CharField(max_length=500)
+  address = models.CharField(max_length=500)
+  # to use ImageField, we need to install Pillow pkg
+  # pip install pillow
+  logo = models.ImageField(upload_to='restaurant_logo/', blank=False)
+
+  def __str__(self):
+    return self.name
--- plateapp/views.py
+++ plateapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from plateapp.forms import UserForm, RestaurantForm
 
 # Create your views here.
 
@@ -16,4 +17,15 @@ def home(request):
 # if authenticated run fn, otherwise go to login_url
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
-  return render(request, 'restaurant/home.html')
+  return render(request, 'restaurant/home.html', {})
+
+# sign-up fn
+def restaurant_sign_up(request):
+  user_form = UserForm()
+  restaurant_form = RestaurantForm()
+  # how do we pass in the forms to the frontend
+  return render(request, 'restaurant/sign_up.html', {
+    # here we pass in the forms
+    "user_form": user_form,
+    "restaurant_form": restaurant_form
+    })

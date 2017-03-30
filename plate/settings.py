32c1e0424dabb66ca20eee40b345a0be89b2839d
antyang <mrant0ny@gmail.com>
2017-03-29 22:18:53 -0700

    [Bug] FacebookOAuth version fix
    * For django-rest.. v1.0.4, change FacebookOAuth2 social_core... -> social...

--- plate/settings.py
+++ plate/settings.py
@@ -142,7 +142,7 @@ DATABASES['default'].update(db_from_env)
 
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.facebook.FacebookOAuth2',
+    'social.backends.facebook.FacebookOAuth2',
     'rest_framework_social_oauth2.backends.DjangoOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
